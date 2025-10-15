c664dd0077be121a0f935030c1b5ae6c9e96381c
mayurgarg08 <mayurgarg2005@gmail.com>
2025-10-16 00:43:07 +0530

    Backend API URL fix

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5001/api";
 
 export const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: "https://videochat-backend-zuzl.onrender.com/api",
   withCredentials: true,
 });
