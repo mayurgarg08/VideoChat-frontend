a3f8a8d03599ea4d5163613793831c4826ce68b3
mayurgarg08 <mayurgarg2005@gmail.com>
2025-10-15 23:50:37 +0530

    updated axios baseURL and env setup

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5001/api";
+
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: BASE_URL,
   withCredentials: true,
 });
